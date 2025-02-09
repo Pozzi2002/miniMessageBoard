3545233441bb3136838109f7469c5f34d4c1da75
Pozzi2002 <nik.stroganov.2019@mail.ru>
2025-02-09 13:34:28 +0300

    Create template and all foulders

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('node:path');
+const indexRouter = require('./routers/index')
+const messageRouter = require('./routers/newMessage')
+
+app.use(indexRouter);
+app.use("/new", messageRouter);
+
+
+
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'ejs');
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log('Server started...'))
